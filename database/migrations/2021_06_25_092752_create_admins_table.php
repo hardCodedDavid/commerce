65ae0814b7328bef2ab9ea79ab8b48ac20759098
devKunle <meezyloaded@gmail.com>
2021-07-18 21:31:13 +0100

    switch colors

--- database/migrations/2021_06_25_092752_create_admins_table.php
+++ database/migrations/2021_06_25_092752_create_admins_table.php
@@ -26,7 +26,7 @@ class CreateAdminsTable extends Migration
         DB::table('admins')
             ->insert([
                 'name' => 'Admin',
-                'email' => 'meezyloaded@gmail.com',
+                'email' => 'admin@marksot.com',
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
             ]);
     }
