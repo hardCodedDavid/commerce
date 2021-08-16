c51f002702aaf5363606c4faf8895a256771dd8d
devKunle <meezyloaded@gmail.com>
2021-08-16 10:18:07 +0100

    make item number unique

--- database/migrations/2021_08_11_155548_add_item_number_to_products_table.php
+++ database/migrations/2021_08_11_155548_add_item_number_to_products_table.php
@@ -14,7 +14,7 @@ class AddItemNumberToProductsTable extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('item_number');
+            $table->string('item_number')->unique();
         });
     }
 
