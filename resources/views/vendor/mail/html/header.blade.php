e74ea5783edfa559bcd111d98424c6a8e12f8c19
GokeAdewuyi <yusufadewuyi2@gmail.com>
2021-09-09 21:55:42 +0100

    update corrections

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -5,7 +5,7 @@
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
 <a href="{{ url('/') }}" style="text-decoration: none">
-    <img src="{{ asset('logo/7.png') }}" style="width: 165px" alt="Marksot Logo">
+    <img src="{{ asset(\App\Models\Setting::first()->email_logo) }}" style="width: 165px" alt="Marksot Logo">
 </a>
 @endif
 </a>
