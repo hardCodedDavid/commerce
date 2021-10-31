25c898e00486a6d7d80a063339ffbb4fa11add3b
GokeAdewuyi <yusufadewuyi2@gmail.com>
2021-10-31 12:19:38 +0100

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\CNSCommand;
+use App\Console\Commands\LogCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        CNSCommand::class
+        CNSCommand::class,
+        LogCommand::class
     ];
 
     /**
